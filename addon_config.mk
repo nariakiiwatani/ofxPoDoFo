69219d937af7e29782db6d6235aba216006a2243
nariakiiwatani <nariakiiwatani@annolab.com>
2019-10-03 00:51:21 +0900

    added license and config

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxPoDoFo
+	ADDON_AUTHOR = nariakiiwatani
+	ADDON_URL = https://github.com/nariakiiwatani/ofxPoDoFo
+
+common:
+	# https://github.com/nariakiiwatani/ofxClipper
+	ADDON_DEPENDENCIES = ofxClipper
